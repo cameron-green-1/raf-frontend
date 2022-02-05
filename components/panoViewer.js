41eb44eef7477145e44aca84c80a5f5e7bc5069c
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2022-02-05 17:29:38 +0000

    remove dat.gui and createCube helper function

--- components/panoViewer.js
+++ components/panoViewer.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-// import { GUI } from 'dat.gui';
 
 import styles from '../styles/PanoViewer.module.css';
 
@@ -34,6 +33,7 @@ const createPano = (imageSrc) => {
 
   // Controls
   const controls = new OrbitControls(camera, webglEl);
+  controls.rotateSpeed = -1; // do they want it to be inverted?
   controls.enablePan = false;
   controls.enableZoom = false;
   controls.autoRotate = false;
@@ -58,50 +58,9 @@ const createPano = (imageSrc) => {
   window.addEventListener('resize', onWindowResize, false);
 };
 
-const createCube = () => {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight
-  );
-
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
-  const webglEl = document.getElementById('sphere');
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  webglEl.appendChild(renderer.domElement);
-
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  cube.position.z = -5;
-  // cube.rotation.x = 10;
-  // cube.rotation.y = 5;
-
-  // const gui = new dat.GUI();
-  // setTimeout(() => console.log(gui), 2000);
-  // console.log(gui);
-  // gui.style.zIndex = 10;
-  // gui.add(cube.rotation, 'x', 0, Math.PI * 2);
-  // gui.add(cube.rotation, 'y', 0, Math.PI * 2);
-  // gui.add(cube.rotation, 'z', 0, Math.PI * 2);
-
-  // renderer.render(scene, camera);
-
-  const animate = () => {
-    cube.rotation.x += 0.01;
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-  };
-
-  animate();
-};
-
 const PanoViewer = ({ imageSrc }) => {
   useEffect(() => {
     createPano(imageSrc);
-    // createCube();
   }, []);
 
   return (
