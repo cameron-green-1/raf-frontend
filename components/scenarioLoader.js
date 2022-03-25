13fb56ae747e1bc1257112db1135c399e9566a89
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2022-03-25 13:11:16 +0000

    add scanning animation to scenario

--- components/scenarioLoader.js
+++ components/scenarioLoader.js
@@ -39,8 +39,11 @@ const Briefing = () => {
   }, []);
   return (
     <div className={styles.briefing}>
-      <img src='/briefing-bg.jpg' className={styles.bg} />
-      <div className={styles.briefingMain}>
+      <div className={styles.bgContainer}>
+        <img src='/briefing-bg.jpg' className={styles.bg} id='bg' />
+        <div className={styles.bgCover} id='bgCover' />
+      </div>
+      <div className={styles.briefingMain} id='briefing-main'>
         <ul className={styles.details}>
           <li className={styles.item}>
             <h1>OPERATION</h1>
@@ -77,6 +80,11 @@ const Briefing = () => {
           </div>
         </div>
       </div>
+      <div className={styles.crosshair} id='crosshair'>
+        <div className={styles.vertical} id='vertical'></div>
+        <div className={styles.horizontal} id='horizontal'></div>
+        <div className={styles.target} id='target'></div>
+      </div>
     </div>
   );
 };
@@ -89,11 +97,78 @@ const ScenarioLoader = () => {
   // const [secondLoading, setSecondLoading] = useState(false);
   // let vid;
   useEffect(() => {
+    const bg = document.getElementById('bg');
+    const bgCover = document.getElementById('bgCover');
+    const crosshair = document.getElementById('crosshair');
+    // const vertical = document.getElementById('vertical');
+    // const horizontal = document.getElementById('horizontal');
+    const target = document.getElementById('target');
+    let vid = document.getElementById('briefing-video');
+
+    // setTimeout(() => {
+    //   setFirstLoading(false);
+    //   if (secondLoading) {
+    //     vid.play();
+    //     vid.muted = false;
+    //     setTimeout(() => {
+    //       vid.muted = true;
+    //       vid.pause();
+    //       setSecondLoading(false);
+    //     }, 5000);
+    //     setTimeout(() => setSecondLoading(false), (vid.duration + 1) * 1000);
+    //   }
+    // }, 3000);
+    // setTimeout(() => setSecondLoading(false), 7000);
+
+    // setTimeout(() => {
+    //   setFirstLoading(false);
+    // if (secondLoading) {
+    //   vid.play();
+    //   vid.muted = false;
+    //   setTimeout(() => {
+    //     vid.muted = true;
+    //     vid.pause();
+    //     setSecondLoading(false);
+    //   }, 5000);
+    //   setTimeout(() => setSecondLoading(false), (vid.duration + 1) * 1000);
+    // }
+    // }, 3000);
+    // setTimeout(() => setSecondLoading(false), 7000);
+
     setTimeout(() => {
       setFirstLoading(false);
-      let vid = document.getElementById('briefing-video');
-      // vid.muted = true;
-      // vid.pause();
+    }, 3000);
+
+    setTimeout(() => {
+      crosshair.style.transform = 'translate(-10vw, -10vh)';
+      setTimeout(() => {
+        target.style.visibility = 'visible';
+        setTimeout(() => {
+          target.style.visibility = 'hidden';
+        }, 750);
+      }, 1250);
+    }, 3250);
+    setTimeout(() => {
+      bg.style.transform = 'translate(22.5%, 22.5%) scale(1.5)';
+    }, 5000);
+    setTimeout(() => {
+      crosshair.style.transform = 'translate(7.5vw, 7.5vh)';
+      setTimeout(() => {
+        target.style.visibility = 'visible';
+        setTimeout(() => {
+          target.style.visibility = 'hidden';
+        }, 750);
+      }, 1250);
+      // setFirstLoading(false);
+    }, 6000);
+    setTimeout(() => {
+      bg.style.transform = 'translate(17.5%, 17.5%) scale(2)';
+    }, 7250);
+    setTimeout(() => {
+      const briefingMain = document.getElementById('briefing-main');
+      crosshair.style.opacity = 0;
+      bgCover.style.transform = 'translateX(120%)';
+      briefingMain.style.opacity = 1;
       if (secondLoading) {
         vid.play();
         vid.muted = false;
@@ -104,14 +179,14 @@ const ScenarioLoader = () => {
         }, 5000);
         setTimeout(() => setSecondLoading(false), (vid.duration + 1) * 1000);
       }
-    }, 3000);
-    // setTimeout(() => setSecondLoading(false), 7000);
-  });
+    }, 8500);
+  }, []);
   return (
     <div
       className={styles.loader}
       style={{
-        transform: secondLoading ? 'translateY(0%)' : 'translateY(-100%)',
+        // transform: secondLoading ? 'translateY(0%)' : 'translateY(-100%)',
+        transform: secondLoading ? 'translateY(0%)' : 'translateY(-200%)',
         pointerEvents: secondLoading ? 'all' : 'none',
       }}
     >
