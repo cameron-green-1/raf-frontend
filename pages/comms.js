dcc56b3746d4b1d709b8adf4805eb3f427c7e0eb
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2022-06-20 17:09:47 +0100

    att 3

--- pages/comms.js
+++ pages/comms.js
@@ -155,71 +155,31 @@ const Comms = ({ launch, live, latest, rooms }) => {
   const [chatRooms, setChatRooms] = useState(null);
   const [latestContent, setLatestContent] = useState(null);
   const [time, setTime] = useState(null);
-
-  const { getConfig, getChatRooms, getLatestContent } = useContentful();
-  const configRetrieved = await getConfig();
-  const roomsRetrieved = await getChatRooms();
-  const latestContentRetrieved = await getLatestContent();
-  if (configRetrieved) {
-    console.log('config retrieved');
-    setConfig(configRetrieved);
-  }
-  if (roomsRetrieved) {
-    console.log('rooms retrieved');
-    setChatRooms(roomsRetrieved);
-    console.log(roomsRetrieved);
-    console.log(chatRooms);
-  }
-  if (latestContentRetrieved) {
-    console.log('latest content retrieved');
-    setLatestContent(latestContentRetrieved);
-    console.log(latestContentRetrieved);
-    console.log(latestContent);
-  }
-  useEffect(() => {
-    // async function getData() {
-    //   const { getConfig, getChatRooms, getLatestContent } = useContentful();
-    //   const configRetrieved = await getConfig();
-    //   const roomsRetrieved = await getChatRooms();
-    //   const latestContentRetrieved = await getLatestContent();
-    //   if (configRetrieved) {
-    //     console.log('config retrieved');
-    //     setConfig(configRetrieved);
-    //   }
-    //   if (roomsRetrieved) {
-    //     console.log('rooms retrieved');
-    //     setChatRooms(roomsRetrieved);
-    //     console.log(roomsRetrieved);
-    //     console.log(chatRooms);
-    //   }
-    //   if (latestContentRetrieved) {
-    //     console.log('latest content retrieved');
-    //     setLatestContent(latestContentRetrieved);
-    //     console.log(latestContentRetrieved);
-    //     console.log(latestContent);
-    //   }
-    // }
-    // getData();
-    // const { getConfig, getChatRooms, getLatestContent } = useContentful();
-    // const configRetrieved = await getConfig();
-    // const roomsRetrieved = await getChatRooms();
-    // const latestContentRetrieved = await getLatestContent();
-    // if (configRetrieved) {
-    //   console.log('config retrieved');
-    //   setConfig(configRetrieved);
-    // }
-    // if (roomsRetrieved) {
-    //   console.log('rooms retrieved');
-    //   setChatRooms(roomsRetrieved);
-    //   console.log(roomsRetrieved);
-    //   console.log(chatRooms);
-    // }
-    // if (latestContentRetrieved) {
-    //   console.log('latest content retrieved');
-    //   setLatestContent(latestContentRetrieved);
-    //   console.log(latestContentRetrieved);
-    //   console.log(latestContent);
-    // }
+  useEffect(async () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { getConfig, getChatRooms, getLatestContent } = useContentful();
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const configRetrieved = await getConfig();
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const roomsRetrieved = await getChatRooms();
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const latestContentRetrieved = await getLatestContent();
+    if (configRetrieved) {
+      console.log('config retrieved');
+      setConfig(configRetrieved);
+    }
+    if (roomsRetrieved) {
+      console.log('rooms retrieved');
+      setChatRooms(roomsRetrieved);
+      console.log(roomsRetrieved);
+      console.log(chatRooms);
+    }
+    if (latestContentRetrieved) {
+      console.log('latest content retrieved');
+      setLatestContent(latestContentRetrieved);
+      console.log(latestContentRetrieved);
+      console.log(latestContent);
+    }
   }, []);
   useEffect(() => {
     const date = new Date(config.launchTime);
